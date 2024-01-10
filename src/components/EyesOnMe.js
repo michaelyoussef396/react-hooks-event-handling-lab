1f24d7d9964cadd1f98db770b74d3579621e3ee9
michael youssef <michaelyoussef396@gmail.com>
2024-01-10 16:45:50 +1100

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function handlefocus(event){
+        console.log('Good!')
+    }
+
+    function handleBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={handlefocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
